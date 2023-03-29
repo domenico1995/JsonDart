8898f4a3d94dd66de564d1d2fe261e6bafa278a3
progect-cloud <progect-cloud@outlook.it>
2023-03-29 15:45:26 +0200

    aggiunta codice per task2

--- web/main.dart
+++ web/main.dart
@@ -1,9 +1,34 @@
 import 'dart:convert';
 import 'dart:html';
+
 //Variabile di collegamento tra <ul> e il codice dart
-final UListElement lista = querySelector('#lista') as UListElement;
+TextAreaElement input = querySelector('#input') as TextAreaElement;
+TextAreaElement output = querySelector('#output') as TextAreaElement;
+ButtonElement pulire = querySelector('#clear_all') as ButtonElement;
+ButtonElement pulire_input = querySelector('#clear_input') as ButtonElement;
+ButtonElement pulire_output = querySelector('#clear_output') as ButtonElement;
+ButtonElement invio = querySelector('#invio') as ButtonElement;
 
 Future<void> main() async {
+  invio.onClick.listen((Event e) {
+    scrivi();
+  });
+  pulire.onClick.listen((Event e) {
+    print("pulire ");
+    input.children.clear();
+    output.children.clear();
+  });
+  pulire_input.onClick.listen((event) {
+    print("hello");
+    input.children.clear();
+  });
+  pulire_output.onClick.listen((event) {
+    output.children.clear();
+  });
+}
+
+Future<void> scrivi() async {
+  print("nome");
   //fileNome contiene il nome del file
   String fileNome = 'test.json';
   //contenutoJson contiene il testo del file
@@ -16,6 +41,7 @@ Future<void> main() async {
     var listElement = LIElement();
     listElement.text =
         m['name'] + ' ' + m['item'] + ' [' + m['lat'] + '] [' + m['lon'] + ']';
-    //lista.children.add(listElement);
+    output.append(listElement);
   }
+  print("he");
 }
