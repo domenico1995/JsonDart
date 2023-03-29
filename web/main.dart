ee214f962d266e4eba2f2c17293be891bb51bcf4
progect-cloud <progect-cloud@outlook.it>
2023-03-29 18:57:12 +0200

    aggiunta commenti

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:html';
 
-//Variabile di collegamento tra <ul> e il codice dart
 TextAreaElement input = querySelector('#input') as TextAreaElement;
 TextAreaElement output = querySelector('#output') as TextAreaElement;
 ButtonElement pulire = querySelector('#clear_all') as ButtonElement;
@@ -11,37 +10,35 @@ ButtonElement invio = querySelector('#invio') as ButtonElement;
 
 Future<void> main() async {
   invio.onClick.listen((Event e) {
+    //test per vedere se è possibile leggere ciò che è presente nell textarea input
+    print(invio.value);
     scrivi();
   });
+  //pulire textarea input e output
   pulire.onClick.listen((Event e) {
-    print("pulire ");
     input.children.clear();
     output.children.clear();
   });
+  //pulire textarea input
   pulire_input.onClick.listen((event) {
-    print("hello");
     input.children.clear();
   });
+  //pulire textarea output
   pulire_output.onClick.listen((event) {
     output.children.clear();
   });
 }
 
 Future<void> scrivi() async {
-  print("nome");
-  //fileNome contiene il nome del file
   String fileNome = 'test.json';
-  //contenutoJson contiene il testo del file
   var contenutoJson = await HttpRequest.getString(fileNome);
-  //crea una lista dalla conversione da json
   var contenuto = jsonDecode(contenutoJson) as List<dynamic>;
-  //ogni elemento della lista viene usato come Map e i suoi elementi dati per essere visulaizzati
   for (int i = 0; i < contenuto.length; i++) {
     var m = contenuto[i] as Map;
-    var listElement = LIElement();
-    listElement.text =
+    var lista = LIElement();
+    lista.text =
         m['name'] + ' ' + m['item'] + ' [' + m['lat'] + '] [' + m['lon'] + ']';
-    output.append(listElement);
+    //dovrebbe aggiungere lista nell'textarea output
+    output.append(lista);
   }
-  print("he");
 }
