15c19f59ac36edb7d5f3d1ccf62d9290ec853a17
progect-cloud <progect-cloud@outlook.it>
2023-03-29 19:03:54 +0200

    aggiunta commenti

--- web/main.dart
+++ web/main.dart
@@ -9,23 +9,29 @@ ButtonElement pulire_output = querySelector('#clear_output') as ButtonElement;
 ButtonElement invio = querySelector('#invio') as ButtonElement;
 
 Future<void> main() async {
+  //test per vedere se è possibile leggere ciò che è presente nell textarea input
+  //i print servono per vedere se i listen funzionano
   invio.onClick.listen((Event e) {
-    //test per vedere se è possibile leggere ciò che è presente nell textarea input
-    print(invio.value);
+    print(invio.value);//da come risultato vuoto, non prende niente
     scrivi();
+    print("prova");
   });
-  //pulire textarea input e output
+  //i metodi successivi dovrebbero pulire le textarea ma non lo fanno, rimangono piene
+  //pulire textarea input e output 
   pulire.onClick.listen((Event e) {
     input.children.clear();
     output.children.clear();
+    print("prova1");
   });
-  //pulire textarea input
+  //pulire textarea input ma non puliscono
   pulire_input.onClick.listen((event) {
     input.children.clear();
+     print("prova2");
   });
   //pulire textarea output
   pulire_output.onClick.listen((event) {
     output.children.clear();
+     print("prova3");
   });
 }
 
@@ -38,7 +44,7 @@ Future<void> scrivi() async {
     var lista = LIElement();
     lista.text =
         m['name'] + ' ' + m['item'] + ' [' + m['lat'] + '] [' + m['lon'] + ']';
-    //dovrebbe aggiungere lista nell'textarea output
+    //dovrebbe aggiungere lista nell'textarea output ma non succede rimane vuota o non cambia
     output.append(lista);
   }
 }
